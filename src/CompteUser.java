86c77c1ac7f95760bb0e6e163855c761620a10a1
melissa sadeg <sadeg@et.esiea.fr>
2024-01-16 15:05:51 +0100

    methode qui verifie la bonne adresse emeil

--- src/CompteUser.java
+++ src/CompteUser.java
@@ -1,2 +1,55 @@
+import java.util.ArrayList;
+
 public class CompteUser {
+    private String Mdp;
+    private String email;
+    private ArrayList<CompteBancaires> comptesBancaire;
+
+    public CompteUser(String mdp, String email, ArrayList<CompteBancaires> comptesBancaire) {
+        Mdp = mdp;
+        this.email = email;
+        this.comptesBancaire = comptesBancaire;
+    }
+
+    public String getMdp() {
+        return Mdp;
+    }
+    public String getEmail(){
+        return email;
+    }
+
+    public ArrayList<CompteBancaires> getComptesBancaire() {
+        return comptesBancaire;
+    }
+    // verifie l'entree de la bonne adresse emeil
+    public static Boolean bonAdresse(String Email) {
+        if (Email == null)
+            return false;
+
+        if  (Email.indexOf('@') == -1 || Email.indexOf('.') == -1)
+            return false;
+
+        if (Email.indexOf('@') == 0 || Email.indexOf('.') < Email.indexOf('@') || Email.length() - 1 == Email.indexOf('.'))
+            return false;
+        if (Email.indexOf('@') + 1 == Email.indexOf('.'))
+            return false;
+
+        else
+            return true;
+
+
+    }
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args ) {
+        String Email = "moussy@gmail.com";
+        System.out.println(CompteUser.bonAdresse(Email));
+    }
 }
